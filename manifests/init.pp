c2d60a7bf5719aa9a8ae0d770362c7d9950d4725
Lee Boynton <lee@lboynton.com>
2012-10-03 16:03:04 +0100

    Add remi repository module

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+class remi($extras = 0) {
+    yumrepo { 'remi':
+        baseurl     => 'http://rpms.famillecollet.com/enterprise/$releasever/remi/$basearch/',
+        mirrorlist  => 'http://rpms.famillecollet.com/enterprise/$releasever/remi/mirror',
+        enabled     => 1,
+        gpgcheck    => 1,
+        gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
+        descr       => 'Les RPM de remi pour Enterprise Linux $releasever - $basearch'
+    }
+    
+    yumrepo { 'remi-extras':
+        baseurl     => 'http://rpms.famillecollet.com/enterprise/$releasever/test/$basearch/',
+        mirrorlist  => 'http://rpms.famillecollet.com/enterprise/$releasever/test/mirror',
+        enabled     => $extras,
+        gpgcheck    => 1,
+        gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
+        descr       => 'Les RPM de remi en test pour Enterprise Linux $releasever - $basearch'
+    }
+    
+    file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi":
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => "puppet:///modules/rpmforge/RPM-GPG-KEY-remi",
+    }
+    
+    epel::rpm_gpg_key{ "remi":
+        path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi",
+    }
+}
