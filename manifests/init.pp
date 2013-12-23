1d339a08bc51918311e2ca9d305b4b4509d6a427
Jeremy Nederhoff <jeremy.nederhoff@gmail.com>
2013-12-23 15:36:20 -0600

    Added remi-php55 repo and bumped version number

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class remi($extras = 0) {
+class remi($extras = 0, $php55 = 0) {
     yumrepo { 'remi':
         baseurl     => 'http://rpms.famillecollet.com/enterprise/$releasever/remi/$basearch/',
         mirrorlist  => 'http://rpms.famillecollet.com/enterprise/$releasever/remi/mirror',
@@ -7,7 +7,16 @@ class remi($extras = 0) {
         gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
         descr       => 'Les RPM de remi pour Enterprise Linux $releasever - $basearch'
     }
-    
+
+    yumrepo { 'remi-php55':
+        baseurl     => 'http://rpms.famillecollet.com/enterprise/$releasever/php55/$basearch/',
+        mirrorlist  => 'http://rpms.famillecollet.com/enterprise/$releasever/php55/mirror',
+        enabled     => $php55,
+        gpgcheck    => 1,
+        gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
+        descr       => 'Les RPM de remi de PHP 5.5 pour Enterprise Linux $releasever - $basearch'
+    }
+
     yumrepo { 'remi-extras':
         baseurl     => 'http://rpms.famillecollet.com/enterprise/$releasever/test/$basearch/',
         mirrorlist  => 'http://rpms.famillecollet.com/enterprise/$releasever/test/mirror',
@@ -16,7 +25,7 @@ class remi($extras = 0) {
         gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
         descr       => 'Les RPM de remi en test pour Enterprise Linux $releasever - $basearch'
     }
-    
+
     file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi":
         ensure => present,
         owner  => 'root',
@@ -24,7 +33,7 @@ class remi($extras = 0) {
         mode   => '0644',
         source => "puppet:///modules/remi/RPM-GPG-KEY-remi",
     }
-    
+
     epel::rpm_gpg_key{ "remi":
         path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi",
     }
